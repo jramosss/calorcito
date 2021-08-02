385306b960bca62f290d03768ecf52163e59006a
jramosss <jramostod@gmail.com>
2021-08-02 00:54:26 -0300

    Tweepy implemented

---
+++ tweet.py
@@ -0,0 +1,21 @@
+from utils import leftingDaysForSummer
+import tweepy as tw
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+KEY = environ["API_KEY"]
+SECRET = environ["API_SECRET_KEY"]
+ACCESS_TOKEN = environ["ACCESS_TOKEN"]
+ACCESS_SECRET = environ["ACCESS_TOKEN_SECRET"]
+
+auth = tw.OAuthHandler(KEY,SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tw.API(auth)
+
+lefting_days = leftingDaysForSummer()
+
+if lefting_days != None:
+    api.update_status(lefting_days)
---
+++ utils.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+Y = datetime.now().year
+SUMMER_DAY = datetime(Y,12,21).date().timetuple().tm_yday
+AUTUMN_DAY = datetime(Y,3,21).date().timetuple().tm_yday
+WINTER_DAY = datetime(Y,6,21).date().timetuple().tm_yday
+SPRING_DAY = datetime(Y,9,21).date().timetuple().tm_yday
+
+class StationsSTR:
+    def __init__(self) -> None:
+        self.SUMMER = "Summer"
+        self.AUTUMN = "Autumn"
+        self.WINTER = "Winter"
+        self.SPRING = "Spring"
+
+
+class Stations:
+    def __init__(self) -> None:
+        self.AUTUMN = range(AUTUMN_DAY,WINTER_DAY)
+        self.WINTER = range(WINTER_DAY,SPRING_DAY)
+        self.SPRING = range(SPRING_DAY,SUMMER_DAY)
+
+def checkTime ():
+    today = datetime.now().timetuple().tm_yday
+    stations = Stations()
+    st = StationsSTR()
+        
+    if today in stations.SPRING:
+        return st.SPRING
+    elif today in stations.AUTUMN:
+        return st.AUTUMN
+    elif today in stations.WINTER:
+        return st.WINTER
+    else:
+        return st.SUMMER
+
+
+def leftingDaysForSummer ():
+    today_number = datetime.now().timetuple().tm_yday
+    today = datetime.now().date()
+
+    st = Stations()
+    summer_date = datetime(Y,12,21).date()
+    if not (today_number in st.SPRING or today_number in st.WINTER or today_number in st.AUTUMN):
+        return None
+    else:
+        return summer_date - today
